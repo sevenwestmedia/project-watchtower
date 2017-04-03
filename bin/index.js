9070d412147eb477a9b12b882e8cb755303d7f13
Michael Strobel <michael.strobel@wanews.com.au>
2017-04-03 13:43:18 +0800

    Add comment to bin/index.js to explain why we can't use TS

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
+
+/*
+ * This file can not be TypeScript as transpiling replace the line endings
+ * with CRLF on Windows, which breks the shebang line on linux/unix!
+ */
+
 require('../lib/bin')
