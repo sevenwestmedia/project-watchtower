5a7a3178e27e056393e3dff8055105c56be65ccf
Michael Strobel <michael.strobel@wanews.com.au>
2017-04-03 10:04:33 +0800

    Fix: Don't clean config folder

--- lib/bin/clean.ts
+++ lib/bin/clean.ts
@@ -7,7 +7,7 @@ const clean = (...paths: string[]) => (
     doClean([
         CLIENT_OUTPUT,
         SERVER_OUTPUT,
-        '{client,common,config,server}/**/*.{js,map}',
+        '{client,common,server}/**/*.{js,map}',
         'assets.json',
         'build-stats.csv',
         'coverage',
