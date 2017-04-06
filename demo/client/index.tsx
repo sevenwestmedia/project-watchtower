8c14914d0e0c26ae86731202cb76ef8034663da2
Michael Strobel <michael.strobel@wanews.com.au>
2017-04-06 13:23:19 +0800

    Update and fix demo

--- demo/client/index.tsx
+++ demo/client/index.tsx
@@ -1,9 +1,10 @@
+import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { cssHotReload } from 'project-watchtower/lib/client/dev'
 
 const render = () => {
     const App = require('../common/App').default
-    ReactDOM.render(App, document.getElementById('app-root'))
+    ReactDOM.render(<App />, document.getElementById('app-root'))
 }
 
 render()
