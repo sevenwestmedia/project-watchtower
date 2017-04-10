d7256b713162d57c7e84d7dba894e5e1ef526092
Michael Strobel <michael.strobel@wanews.com.au>
2017-04-10 08:52:50 +0800

    Lint: use all paths when running a single linter

--- lib/bin/lint.ts
+++ lib/bin/lint.ts
@@ -4,19 +4,21 @@ import { log } from '../util/log'
 
 /**
  * Runs TSLint and/or SASS-Lint
- * Paths have to contain ".ts" or ".scss" to be mapped correctly
+ * Paths have to contain ".ts" or ".scss" to be mapped correctly if both linters are called
  */
 const lint = (...paths: string[]) => {
-    const tslintPaths = paths.filter((p) => p.indexOf('.ts') !== -1)
-    const sassLintPaths = paths.filter((p) => p.indexOf('.scss') !== -1)
-
     let promise: Promise<any>
 
     if (paths.indexOf('tslint') !== -1) {
+        const tslintPaths = paths.filter((p) => p !== 'tslint')
         promise = tslint(...tslintPaths)
     } else if (paths.indexOf('sass-lint') !== -1) {
+        const sassLintPaths = paths.filter((p) => p !== 'sass-lint')
         promise = sassLint(...sassLintPaths)
     } else {
+        const tslintPaths = paths.filter((p) => p.indexOf('.ts') !== -1)
+        const sassLintPaths = paths.filter((p) => p.indexOf('.scss') !== -1)
+
         promise = Promise.all([
             tslint(...tslintPaths),
             sassLint(...sassLintPaths),
