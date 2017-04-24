8f7e2edce8478b3d4959c7b9aab9692ab89e0e67
Michael Strobel <michael.strobel@wanews.com.au>
2017-04-24 14:37:34 +0800

    Fix test type error

--- test/__test-data__/process-exit.ts
+++ test/__test-data__/process-exit.ts
@@ -2,5 +2,5 @@
  * This file exits the process with a return code supplied
  * as CLI argument to test erroring child processes
  */
-const returnCode = parseInt(process.argv[2] || 0, 10)
+const returnCode = parseInt(process.argv[2] || '0', 10)
 process.exit(returnCode)
