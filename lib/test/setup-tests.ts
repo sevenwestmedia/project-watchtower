850e99b1e8e7a8cfc638c86512af16775dacbb3f
Michael Strobel <michael.strobel@wanews.com.au>
2017-04-26 11:32:41 +0800

    Remove logging for using custom test setup file

--- lib/test/setup-tests.ts
+++ lib/test/setup-tests.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import * as dotenv from 'dotenv'
-import { log } from '../util/log'
 
 dotenv.config()
 process.env.NODE_ENV = 'test'
@@ -10,7 +9,6 @@ const root = process.cwd()
 const testSetupFile = path.resolve(root, 'config', 'test-setup')
 
 if (fs.existsSync(testSetupFile + '.ts') || fs.existsSync(testSetupFile + '.js')) {
-    log('Using custom test setup file /config/test-setup')
     // tslint:disable-next-line no-var-requires
     require(testSetupFile)
 }
