99f97391ccb732549d8ba93f10724afb67a480f8
Michael Strobel <michael.strobel@wanews.com.au>
2017-06-09 08:54:59 +0800

    Upgrade demo to project-watchtower 0.4.0

--- demo/server/index.ts
+++ demo/server/index.ts
@@ -1,48 +1,36 @@
 import * as React from 'react'
-import * as express from 'express'
 import { renderToString } from 'react-dom/server'
-import { addAssetsToHtml } from 'project-watchtower/lib/server/assets'
-import { getHotReloadMiddleware, openBrowser } from 'project-watchtower/lib/server/dev'
+
+import {
+    createServer,
+    getCssAssetHtml,
+    getJsAssetHtml,
+} from 'project-watchtower/lib/runtime/server'
+
 import App from '../common/App'
 
-const port = process.env.PORT || 3000
-const isProduction = process.env.NODE_ENV === 'production'
-const watchMode = process.env.START_WATCH_MODE === 'true'
+const html = (ssr: string) => (
+`<!DOCTYPE html>
+<html>
+<head>
+    <title>Demo Server</title>
+    ${getCssAssetHtml()}
+</head>
+<body>
+    <div id="app-root">${ssr}</div>
+    ${getJsAssetHtml()}
+</body>
+</html>`
+)
 
-const createServer = () => {
-    const app = express()
-
-    if (!isProduction && watchMode) {
-        app.use(getHotReloadMiddleware())
-    }
-
-    app.use(express.static('public', {
-        index: false,
-    }))
-
-    const ssr = renderToString(React.createElement(App))
-
-    app.get('*', (_req, res) => {
-        const content = addAssetsToHtml(`<!DOCTYPE html>
-    <html>
-    <head>
-        <title>Demo Server</title>
-    </head>
-    <body>
-        <div id="app-root">${ssr}</div>
-    </body>
-    </html>`)
-
-        res.send(content)
-    })
-
-    app.listen(port, () => {
-        // tslint:disable-next-line no-console
-        console.log(`Server listening on port ${port}`)
-        if (!isProduction && watchMode) {
-            openBrowser(port)
-        }
+const createAppServer = () => {
+    createServer((app) => {
+        app.get('*', (_req, res) => {
+            const ssr = renderToString(React.createElement(App))
+            const content = html(ssr)
+            res.send(content)
+        })
     })
 }
 
-export default createServer
+export default createAppServer
