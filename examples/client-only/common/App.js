5c8e0c038e5cb67ba4caf63438910a4cb090591d
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2018-08-27 15:53:04 +0800

    Deleted files added before .gitignore

--- examples/client-only/common/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-require("./App.scss");
-exports.App = function () { return (React.createElement("h1", null,
-    "Hello world",
-    React.createElement("img", { src: require('./thumb.png') }))); };
-exports.default = exports.App;
-//# sourceMappingURL=App.js.map
