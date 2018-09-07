8663388c44447f6a160898d88841315f238606b3
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2018-09-07 09:21:09 +0800

    Cleaned up transfer state so it returns possibly undefined

--- lib/runtime/client/get-transferred-state.ts
+++ lib/runtime/client/get-transferred-state.ts
@@ -2,6 +2,6 @@
  * Returns the state transferred by the transferState function
  * @param key The key to use to read the state
  */
-export function getTransferredState<T = any>(key: string) {
+export function getTransferredState<T = any>(key: string): T | undefined {
     return typeof window !== 'undefined' ? ((window as any)[key] as T) : undefined
 }
