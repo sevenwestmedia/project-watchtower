951e98c358b2342cecec97d68fc35de613baf287
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2018-09-07 09:16:43 +0800

    Fixed some import issues

--- lib/runtime/client/get-transferred-state.ts
+++ lib/runtime/client/get-transferred-state.ts
@@ -3,5 +3,5 @@
  * @param key The key to use to read the state
  */
 export function getTransferredState<T = any>(key: string) {
-    return (window as any)[key] as T
+    return typeof window !== 'undefined' ? ((window as any)[key] as T) : undefined
 }
