dd7c49777be216e4a5241c64de74a1e71da7c0ed
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2018-09-17 22:32:42 +0800

    Cleaned up unneeded hydration code

--- examples/emotion-css-in-js/client/index.tsx
+++ examples/emotion-css-in-js/client/index.tsx
@@ -1,17 +1,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { cssHotReload } from '../../../lib/runtime/client'
-import { getTransferredState } from '../../../lib/runtime/client/get-transferred-state'
-import { hydrate } from 'emotion'
 
 import { App } from '../common/App'
-
-// We can get the transferred state by using the getTransferredState function
-const emotionIds = getTransferredState<string[]>('_emotionIds')
-
-if (emotionIds) {
-    hydrate(emotionIds)
-}
 
 const render = () => {
     ReactDOM.hydrate(<App />, document.getElementById('root'))
