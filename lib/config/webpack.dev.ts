cf48ad3bc77ad8690e908b8ce2e4e1077bcd4c0a
Sebastian Dopierala <intelseb@gmail.com>
2018-10-18 15:59:41 +0800

    Some more changes

--- lib/config/webpack.dev.ts
+++ lib/config/webpack.dev.ts
@@ -2,12 +2,9 @@ import webpack from 'webpack'
 
 /** Webpack settings for all development builds */
 const devConfig: webpack.Configuration = {
+    mode: 'development',
     devtool: 'cheap-module-source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development'),
-        }),
-    ],
+    plugins: [],
 }
 
 export default devConfig
--- lib/config/webpack.prod.ts
+++ lib/config/webpack.prod.ts
@@ -1,30 +1,36 @@
 import webpack from 'webpack'
 import { CreateWebpackConfig } from './index'
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
 
 /** Webpack settings for all production builds */
 const prodConfig: CreateWebpackConfig = () => ({
+    mode: 'production',
     devtool: 'source-map',
     resolve: {
         alias: {
             'project-watchtower/lib': 'project-watchtower/dist-es',
         },
     },
+    optimization: {
+        minimizer: [
+            // Might need to remove this, as minimizer on by default? Not sure if it does source maps
+            new UglifyJsPlugin({
+                // compress: {
+                //     screw_ie8: true,
+                //     warnings: false,
+                // },
+                // mangle: {
+                //     screw_ie8: true,
+                // },
+                sourceMap: true,
+            }),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+    },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
-        }),
         new webpack.optimize.AggressiveMergingPlugin(),
         new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                screw_ie8: true,
-                warnings: false,
-            },
-            mangle: {
-                screw_ie8: true,
-            },
-            sourceMap: true,
-        }),
         new webpack.LoaderOptionsPlugin({
             minimize: true,
         }),
