4acb058cb58570b8a8966acef50a754b7613233a
Seb Dopierala <sebastian.dopierala@wanews.com.au>
2018-10-19 12:27:37 +0800

    Fixed test

--- test/util/webpack.ts
+++ test/util/webpack.ts
@@ -1,18 +1,16 @@
-// import webpack from 'webpack'
-// import { printWebpackStats } from '../../lib/util/webpack'
-// import { createConsoleLogger } from '../../lib/runtime/universal'
+import { printWebpackStats } from '../../lib/util/webpack'
+import { createConsoleLogger } from '../../lib/runtime/universal'
 
-// const log = createConsoleLogger()
+const log = createConsoleLogger()
 
-// describe('util/webpack', () => {
-//     it('printWebpackStats', () => {
-//         const stats: webpack.Stats = {
-//             toString: () => 'stats',
-//             hasErrors: () => false,
-//             hasWarnings: () => false,
-//             toJson: () => "{ stats: 'as json' }",
-//         }
-
-//         printWebpackStats(log, stats)
-//     })
-// })
+describe('util/webpack', () => {
+    it('printWebpackStats', () => {
+        const stats: any = {
+            toString: () => 'stats',
+            hasErrors: () => false,
+            hasWarnings: () => false,
+            toJson: () => "{ stats: 'as json' }",
+        }
+        printWebpackStats(log, stats)
+    })
+})
