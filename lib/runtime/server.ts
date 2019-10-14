5347ad6b7c15a2042b2b2790cb515f1cd9f61c58
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-10-14 19:55:25 +0800

    Improved docs

--- lib/runtime/server.ts
+++ lib/runtime/server.ts
@@ -65,7 +65,7 @@ export interface BuildConfig {
     /** if true, all externals will be bundled */
     SERVER_BUNDLE_ALL: boolean
 
-    /** Include all modules, except a blacklist */
+    /** Include all modules, except those specified */
     SERVER_BUNDLE_ALL_EXCEPT?: string[]
 
     /** modules which the server build includes in the bundle */
