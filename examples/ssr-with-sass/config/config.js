c2035c57b7cb55d60cd5c53a2d55cb19508e6065
Charles <charles.cullen@wanews.com.au>
2024-06-05 10:53:46 +0800

    Cleaned up ssr-with-sass to compile successfully

--- examples/ssr-with-sass/config/config.js
+++ examples/ssr-with-sass/config/config.js
@@ -1,5 +1,24 @@
 // eslint-disable-next-line no-undef
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
 module.exports = {
+    // Configuration options for css-minimizer-webpack-plugin
+    module: {
+        rules: [
+            {
+                test: '/.s?css$/',
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    optimization: {
+        minizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+
     default: {
         CLIENT_ENTRY: './examples/ssr-with-sass/src/client/index.tsx',
         SERVER_ENTRY: './examples/ssr-with-sass/src/server/start.ts',
