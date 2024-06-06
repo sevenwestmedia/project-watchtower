682dc6dbb588f9d9a6ccc647c29300e2ed512bb8
Charles <charles.cullen@wanews.com.au>
2024-06-06 16:55:00 +0800

    Continued to fix up some commands to run on Node 18

--- examples/emotion-css-in-js/config/config.js
+++ examples/emotion-css-in-js/config/config.js
@@ -2,6 +2,6 @@
 exports.default = {
     CLIENT_ENTRY: './examples/emotion-css-in-js/src/client/index.tsx',
     SERVER_ENTRY: './examples/emotion-css-in-js/src/server/start.ts',
-    TS_CONFIG_CLIENT: 'examples/emotion-css-in-js/src/tsconfig.json',
-    TS_CONFIG_SERVER: 'examples/emotion-css-in-js/src/tsconfig.json',
+    TS_CONFIG_CLIENT: 'examples/emotion-css-in-js/tsconfig.json',
+    TS_CONFIG_SERVER: 'examples/emotion-css-in-js/tsconfig.json',
 }
