16795a816bb72bcf23fb4e00d7964aef705b158e
Charles <charles.cullen@wanews.com.au>
2024-06-07 11:42:44 +0800

    Removed file-loader and utilised webpack5's asset modules on advice of @bennettp123

--- packages/cli/src/config/webpack.base.ts
+++ packages/cli/src/config/webpack.base.ts
@@ -1,16 +1,5 @@
 import webpack from 'webpack'
-import { BuildConfig } from '@project-watchtower/server'
 import { CreateWebpackConfigOptions } from '.'
-
-export const fileLoaderConfig = (buildConfig: BuildConfig) => ({
-    exclude: /\/favicon.ico$/,
-    loader: 'file-loader',
-    options: {
-        name: buildConfig.ASSETS_PATH_PREFIX + 'media/[name].[hash:8].[ext]',
-        esModule: false,
-    },
-    test: /\.(ico|jpg|png|gif|otf|webp|svg|ttf)(\?.*)?$/,
-})
 
 const plugins = [new webpack.NoEmitOnErrorsPlugin()]
 
@@ -24,14 +13,19 @@ export function baseConfig(options: CreateWebpackConfigOptions): webpack.Configu
     return {
         module: {
             rules: [
-                fileLoaderConfig(options.buildConfig),
                 {
-                    loader: 'file-loader',
-                    options: {
-                        name: options.buildConfig.ASSETS_PATH_PREFIX + 'fonts/[name].[ext]',
-                        esModule: false,
-                    },
+                    test: /\.(ico|jpg|png|gif|otf|webp|svg|ttf)(\?.*)?$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: options.buildConfig.ASSETS_PATH_PREFIX + 'media/[name].[hash:8].[ext]'
+                    }
+                },
+                {
                     test: /\.(eot|woff|woff2)(\?.*)?$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: options.buildConfig.ASSETS_PATH_PREFIX + 'fonts/[name].[ext]',
+                    },
                 },
             ],
         },
